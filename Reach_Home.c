6065d58158b8d961ea46443f0c362c7cb6bf2446
Sriveniyellaboyina <147792246+Sriveniyellaboyina@users.noreply.github.com>
2023-10-15 20:44:05 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d%d",&x,&y);     if(5*x>y)     {         printf("YES");     }     else if(5*x==y)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Reach_Home.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    if(5*x>y)
+    {
+        printf("YES");
+    }
+    else if(5*x==y)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
