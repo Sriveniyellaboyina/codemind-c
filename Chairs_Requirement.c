d57c3f3a37d1e339c977d9a59d1fd03c564e8565
Sriveniyellaboyina <147792246+Sriveniyellaboyina@users.noreply.github.com>
2023-12-05 18:37:37 +0530

    #include<stdio.h> int main() {     int x,y,res;     scanf("%d%d",&x,&y);     if(res=x-y)     {         printf("%d",res);     }     else     {         printf("0");     } }

---
+++ Chairs_Requirement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,res;
+    scanf("%d%d",&x,&y);
+    if(res=x-y)
+    {
+        printf("%d",res);
+    }
+    else
+    {
+        printf("0");
+    }
+}
