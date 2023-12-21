51f82c2cddd9fa396552f60531f2a4b74e07231e
Sriveniyellaboyina <147792246+Sriveniyellaboyina@users.noreply.github.com>
2023-12-21 09:01:36 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=65;j<n+65;j++)         {             printf("%c ",j);         }         printf(" ");     } }

---
+++ Basic_Pattern_1.4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=65;j<n+65;j++)
+        {
+            printf("%c ",j);
+        }
+        printf("
+");
+    }
+}
