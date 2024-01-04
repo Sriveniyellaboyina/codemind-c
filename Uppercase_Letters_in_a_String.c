e8c4e8dba938cdf9fca948bfaaeb14d228e69a2c
Sriveniyellaboyina <147792246+Sriveniyellaboyina@users.noreply.github.com>
2024-01-04 23:10:38 +0530

    #include<stdio.h> #include<string.h> int main() {     char c[65];     int i,k=0;     scanf("%[^ ]s",c);     for(i=0;c[i]!=NULL;i++)     {         if(c[i]>=65 && c[i]<=90)         {             k++;         }     }     printf("%d",k); }

---
+++ Uppercase_Letters_in_a_String.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char c[65];
+    int i,k=0;
+    scanf("%[^
+]s",c);
+    for(i=0;c[i]!=NULL;i++)
+    {
+        if(c[i]>=65 && c[i]<=90)
+        {
+            k++;
+        }
+    }
+    printf("%d",k);
+}
